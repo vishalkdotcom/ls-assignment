a2576e46c6f58554f193ba1af11297da2a0250dc
Vishal Kumar <me@vishalk.com>
2024-02-19 21:23:31 +0530

    display spinner while loading persisted redux state first time

--- src/providers/store-provider.tsx
+++ src/providers/store-provider.tsx
@@ -6,6 +6,7 @@ import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 
 import { makeStore, AppStore } from "@/lib/store";
+import { Box, CircularProgress, Grid, Typography } from "@mui/material";
 
 export default function StoreProvider({ children }: React.PropsWithChildren) {
   const storeRef = React.useRef<AppStore>();
@@ -17,11 +18,31 @@ export default function StoreProvider({ children }: React.PropsWithChildren) {
   return (
     <Provider store={storeRef.current}>
       <PersistGate
-        loading={<div> Loading...</div>}
+        loading={<Loader />}
         persistor={persistStore(storeRef.current)}
       >
         {children}
       </PersistGate>
     </Provider>
+  );
+}
+
+function Loader() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        height: "100vh",
+      }}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <CircularProgress />
+      <Typography variant="body2" sx={{ mt: 2, ml: 1.5 }}>
+        Loading your tweets...
+      </Typography>
+    </Box>
   );
 }
